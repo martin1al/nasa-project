3e77fb55476b5a011bf5c038e82c2c519b821fc6
martin1al <mamcorreo@gmail.com>
2022-07-21 15:59:28 +0200

    202

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL = 'mongodb+srv://nasa-api:OKUygrVBHYTgRTYU2@nasacluster.z8nkc.mongodb.net/?retryWrites=true&w=majority';
+require('dotenv').config();
+
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
     console.log('MongoDB connection ready!');
